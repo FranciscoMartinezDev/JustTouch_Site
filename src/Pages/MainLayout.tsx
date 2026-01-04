05f515c447090f6ed0cd43b9fe8a97a2ee7850a7
FranciscoMartinezDev <frncc951@gmail.com>
2026-01-04 04:01:29 -0300

    inicio desarrollo responsive

--- src/Pages/MainLayout.tsx
+++ src/Pages/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
-import { Button, Dropdown, Layout, Menu, Typography, Image } from "antd";
-import { FaBars } from "react-icons/fa6";
+import { Button, Dropdown, Layout, Menu, Typography, Image, Grid, Drawer, Flex } from "antd";
+import { FaAngleLeft, FaBars } from "react-icons/fa6";
 import './MainLayout.scss';
 import { UseAppContext } from "@/Context/AppContext";
 import { viewMap } from "./Pager";
@@ -8,8 +8,10 @@ import { viewMap } from "./Pager";
 const { Header, Sider, Content } = Layout;
 const { Text } = Typography;
 
-
+const { useBreakpoint } = Grid;
 export const MainLayout: FC = () => {
+    const screens = useBreakpoint();
+
     const { sideItems, items } = UseAppContext();
     const [collapsed, setCollapsed] = useState<boolean>(true);
 
@@ -18,20 +20,47 @@ export const MainLayout: FC = () => {
     return (
         <Layout className="main-layout">
             <Header className="layout-header">
-                <Image preview={false}
-                    width={60}
-                    src="https://dvvlhkouasxqzmaxkvll.supabase.co/storage/v1/object/public/footages/JustTouchLogo.png" />
-                <Text>Just Touch</Text>
+                {screens.xxl || screens.xl || screens.lg || screens.md ?
+                    <Image preview={false}
+                        width={60}
+                        src="https://dvvlhkouasxqzmaxkvll.supabase.co/storage/v1/object/public/footages/JustTouchLogo.png" />
+                    : null}
+                {screens.xxl || screens.xl || screens.lg ? <Text>Just Touch</Text> : null}
                 <Button type="text" icon={<FaBars />} onClick={() => setCollapsed(!collapsed)} />
                 <Dropdown menu={{ items }} placement="bottomRight">
                     <Button>Usuario</Button>
                 </Dropdown>
             </Header>
             <Layout>
-                <Sider className="layout-sider" trigger={null} collapsible collapsed={collapsed}>
-                    <Menu mode="inline" defaultSelectedKeys={['1']}
-                        items={sideItems} />
-                </Sider>
+                {screens.xxl || screens.xl || screens.lg || screens.md ?
+                    <Sider className="layout-sider" trigger={null} collapsible collapsed={collapsed}>
+                        <Menu mode="inline" defaultSelectedKeys={['1']}
+                            items={sideItems} />
+                    </Sider>
+                    : <Drawer
+                        className="floating-sider"
+                        title={
+                            <Flex align="center" gap={20}>
+                                <Button icon={<FaAngleLeft />}
+                                    onClick={() => setCollapsed(!collapsed)}
+                                    style={{ backgroundColor: 'inherit', border: 'none', color: 'white', fontSize: 20 }} />
+                                <Image preview={false}
+                                    width={70}
+                                    src="https://dvvlhkouasxqzmaxkvll.supabase.co/storage/v1/object/public/footages/JustTouchLogo.png" />
+                            </Flex>
+                        }
+                        placement={'left'}
+                        closable={false}
+                        open={collapsed}
+                        size={256}
+                    // resizable={{
+                    //     onResize: (newSize) => setSize(newSize),
+                    // }}
+                    >
+                        <p>Drag the edge to resize the drawer</p>
+                        <p>Current size: 256px</p>
+                    </Drawer>}
+
                 <Content className="layout-content">
                     {viewMap['Menu']()}
                 </Content>
--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -1,10 +1,11 @@
 import { FC, ReactNode } from "react";
 import { Page } from "@/Pages/Page";
-import { Button, Checkbox, Collapse, Flex, Space, Tag, Typography } from "antd";
+import { Button, Checkbox, Collapse, Flex, Space, Tag, Typography, ConfigProvider, Grid } from "antd";
 import { FaRegSquarePlus, FaRegRectangleXmark, FaRegPenToSquare, FaAngleDown } from "react-icons/fa6";
 import './Menu.scss';
 
 const { Text } = Typography;
+const { useBreakpoint } = Grid;
 
 
 export const Menu: FC = () => {
@@ -22,44 +23,66 @@ export const Menu: FC = () => {
 
 
 const Catalog: FC = () => {
+    const screens = useBreakpoint();
+
+    const catalogFontSize = screens.xxl || screens.xl || screens.lg || screens.md ? 20 : screens.sm ? 20 : screens.xs ? 15 : 10;
 
     const CatalogHeader: FC = () => (
-        <Flex align="center" className="catalog">
-            <Text>Catalogo 1</Text>
-            <Space style={{ marginLeft: 'auto', color: 'white' }}>
-                <Button color="danger" variant="solid" icon={<FaRegRectangleXmark />} />
-                <Button color="gold" variant="solid" icon={<FaRegPenToSquare />} />
-            </Space>
-        </Flex>
-    )
-
-    const ProductList: FC = () => (
-        <Flex vertical>
-            <Flex className="product">
-                <Text>Producto 1</Text>
-                <Space style={{ marginLeft: 'auto' }}>
-                    <Text>$ 50.50</Text>
-                    <Checkbox checked={true}>
-                        <Tag style={{ color: 'white' }} variant="solid" color={'green'}>Disponible</Tag>
-                    </Checkbox>
+        <ConfigProvider theme={{
+            token: {
+                fontSize: catalogFontSize
+            },
+        }}>
+            <Flex align="center" className="catalog">
+                <Text>Catalogo 1</Text>
+                <Space style={{ marginLeft: 'auto', color: 'white' }}>
+                    <Button color="danger" variant="solid" icon={<FaRegRectangleXmark />} />
+                    <Button color="gold" variant="solid" icon={<FaRegPenToSquare />} />
+                    <Button className="btn-dropdown" variant="solid" style={{
+                    margin: 'auto', border: 'none', color: 'gray'
+                }} icon={<FaAngleDown />} />
                 </Space>
             </Flex>
-        </Flex>
+        </ConfigProvider>
+    )
+
+    const productFontSize = screens.xxl || screens.xl || screens.lg || screens.md ? 15 : screens.sm ? 12 : 10;
+    const ProductList: FC = () => (
+        <ConfigProvider theme={{
+            token: {
+                fontSize: productFontSize
+            }
+        }}>
+            <Flex vertical>
+                <Flex className="product">
+                    <Text>Producto 1</Text>
+                    <Space style={{ marginLeft: 'auto' }}>
+                        <Text>$ 50.50</Text>
+                        <Checkbox checked={true}>
+                            <Tag style={{ color: 'white' }} variant="solid" color={'green'}>Disponible</Tag>
+                        </Checkbox>
+                    </Space>
+                </Flex>
+            </Flex>
+        </ConfigProvider>
     )
 
 
 
     return (
-        <Collapse defaultActiveKey={['1']}
-            expandIconPlacement={'end'}
-            expandIcon={(): ReactNode => <Button variant="solid" style={{
-                margin: 'auto', height: 45,
-                width: 45, border: 'none', fontSize: '1.2rem', color: 'gray'
-            }} icon={<FaAngleDown />} />}
-            items={[
-                {
-                    label: <CatalogHeader />,
-                    children: <ProductList />
-                }]} />
+        <ConfigProvider theme={{
+            token: {
+                fontSize: catalogFontSize
+            }
+        }}>
+            <Collapse defaultActiveKey={['1']}
+                expandIconPlacement={'end'}
+                expandIcon={(): ReactNode => null}
+                items={[
+                    {
+                        label: <CatalogHeader />,
+                        children: <ProductList />
+                    }]} />
+        </ConfigProvider>
     )
 }
